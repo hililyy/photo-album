5b68112461700711d9bf8b55c6f25dca3dd666af
hililyy <joun406@gmail.com>
2023-02-22 21:37:44 +0900

    init

---
+++ PhotoAlbum/PhotoAlbum/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PhotoAlbum
+//
+//  Created by 강조은 on 2023/02/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
